838147087642d02fc2ac1a752dfe38fcc5a55315
MansiAyer <42694815+MansiAyer@users.noreply.github.com>
2021-10-14 09:47:49 +0530

    Update main.py

--- update/main.py
+++ update/main.py
@@ -1,2 +1,39 @@
-with open("../sample.md","w") as file:
-    file.write("#testing md generation ##line two")
+from bs4 import BeautifulSoup as bs
+import requests
+import re
+
+starspage = requests.get('https://github.com/MansiAyer?tab=stars')
+soup = bs(starspage.text, 'html.parser')
+
+
+listitems = []
+for x in soup.find_all("div", class_="col-12 d-block width-full py-4 border-bottom color-border-secondary"):
+		try:
+			title = str(x.h3)
+		except:
+			continue
+		try:
+			description = str(x.p)
+		except:
+			continue
+		try:
+			proglang = str(x.find_all("span", class_="ml-0 mr-3"))
+		except:
+			continue
+		temp = str(title+": <sup>"+proglang+"</sup><span>"+description+"</span>\n<br>\n\n")
+		listitems.append(temp)
+
+temp = "<div align='center'> :octocat: <i> :jack_o_lantern: </i> :octocat: </div><br><details><summary><sub>:octocat: Recently Starred Repos :octocat:</sub></summary><hr><i>"
+tootitems = [temp]
+for x in listitems:
+		a = x.replace("<h3>","<b>")
+		a = a.replace("</h3>","</b>")
+		a = a.replace("None","<br>No description provided :/<br>")
+		a = a.replace("[]","")
+		tootitems.append(a)
+tootitems.append("</i></details>")
+
+with open('../README.md','w',encoding="utf-8") as file:
+	for x in tootitems:
+		file.write(str(x))
+		file.write("\n")
