3220b310edfd8125edd7164241aac1865b5207b6
MansiAyer <42694815+MansiAyer@users.noreply.github.com>
2021-10-17 07:54:41 +0530

    Update main.py

--- update/main.py
+++ update/main.py
@@ -2,7 +2,7 @@ from github import Github
 import sys
 
 g = Github(sys.argv[1])
-listitems = g.get_user().get_starred()
+listitems = g.get_user('MansiAyer').get_starred()
 
 for x in listitems:
 		try:
